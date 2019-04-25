173e8e1db5aff54585d3db2dc3fff1ac01a05f4c
Александр <@DESKTOP-H6VTLT3>
2019-04-25 15:06:38 +0300

    Работа со считывателем

---
+++ RFIDApp/main_form.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace RFIDApp
+{
+    partial class main_form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(224, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Подключиться к считывателю";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(224, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(242, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 86);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(224, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Отключиться от считывателя";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 115);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(390, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Считать";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 172);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(776, 266);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(437, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(143, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "33";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(434, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Сколько вешать в граммах";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 143);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(390, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Считать в таймере";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // main_form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "main_form";
+            this.Text = "Приложение работы с RFID-метками";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ RFIDApp/main_form.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+namespace RFIDApp
+{
+    public partial class main_form : Form
+    {
+        public main_form()
+        {
+            InitializeComponent();
+        }
+        public static SerialPort port = new SerialPort();  // Глобальная переменная 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!port.IsOpen)
+            {
+                try
+                {                                            // настройки порта
+                    port.PortName = comboBox1.SelectedItem.ToString();
+                    port.BaudRate = 38400;
+                    port.DataBits = 8;
+                    port.Parity = Parity.None;
+                    port.StopBits = StopBits.One;
+                    port.ReadTimeout = 1000;
+                    port.WriteTimeout = 1000;
+                    port.Open();
+                    SerialDataReceivedEventHandler SerialPort_DataReceived = null;
+                    port.DataReceived += SerialPort_DataReceived;
+                    label1.Text = "Порт открыт.";
+                }
+                catch (Exception ex)
+                {
+                   MessageBox.Show("ERROR: невозможно открыть порт:" + ex.ToString());
+                    return;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Порт закрыт.";
+            // Получаем список портов
+            string[] ports = SerialPort.GetPortNames();
+            // Заносим список портов в comboBox
+            for (int i = 0; i < ports.Length; i++)
+            {
+                comboBox1.Items.Add(ports[i].ToString());
+            }
+            comboBox1.SelectedIndex = 0; //Выбираем COM3 чтобы быстрее было
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (port.IsOpen)
+            {
+                label1.Text = "Порт закрыт.";
+                port.Close();
+            }
+        }
+        public string ReadFromCOM()
+        {
+            string temp = "";
+            if (port.IsOpen)
+            {
+
+                try
+                {
+
+                    byte[] bMas = { 0xa0, 0x04, 0x01, 0x89, 0x01, 0xd1 };
+                    port.Write(bMas, 0, bMas.Length);
+                    byte[] data = new byte[Convert.ToInt32(textBox1.Text)];
+                    port.Read(data, 0, data.Length);
+
+                    for (int i = 0; i < Convert.ToInt32(textBox1.Text); i++)
+                        temp += data[i].ToString() + " ";
+                }
+                catch (Exception ex)
+                {
+                    return ("ERROR: невозможно открыть порт:" + ex.ToString());
+                }
+            }
+            else
+            {
+                temp = "Открой порт сапака!";
+            }
+            return DateTime.Now.Hour.ToString() + ":" + DateTime.Now.Minute.ToString() + ":"+ DateTime.Now.Second.ToString() + ":" + DateTime.Now.Millisecond.ToString() + ">>" + temp + "\r\n";
+        }
+        
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.AppendText(ReadFromCOM());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled == true)
+                timer1.Stop();
+            else
+                timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            richTextBox1.AppendText(ReadFromCOM());
+        }
+    }
+}
